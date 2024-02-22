2bb3f4e00de4f5b8bbea5868e2c8e8954b74cbad
Diegoredeti <diegoredeti@gmail.com>
2024-02-22 11:35:27 -0300

    ok

---
+++ 02/filtro.ts
@@ -0,0 +1,14 @@
+const filtrarUsuarios = (
+  usuarios: {
+    nome: string;
+    idade: number;
+    status: boolean;
+  }[],
+  usuario: string
+): {
+  nome: string;
+  idade: number;
+  status: boolean;
+}[] => {
+  return [];
+};
